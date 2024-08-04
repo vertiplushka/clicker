5e01516f063249e3616fe5cb4af4b5daca0e5650
vertiplushka <75839950+vertiplushka@users.noreply.github.com>
2024-08-05 00:20:46 +0700

    fix

--- nextjs/app/clicker/layout.js
+++ nextjs/app/clicker/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
